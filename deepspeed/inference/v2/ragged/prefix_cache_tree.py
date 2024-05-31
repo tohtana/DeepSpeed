6eaa7d84d9f8244fbd9986704e278e9b552caa57
Umesh <umchand@microsoft.com>
2024-05-31 17:55:26 +0000

    prefix cache tree fix

--- deepspeed/inference/v2/ragged/prefix_cache_tree.py
+++ deepspeed/inference/v2/ragged/prefix_cache_tree.py
@@ -20,12 +20,12 @@ def sequence_to_state(token_ids: Optional[torch.Tensor] = None):
 class SequenceStateNode:
     
     def __init__(self, block_address: Optional[int] = None,
-                 hash_token_chunk: Optional[torch.Tensor] = None):
+                 hash_token_chunk: Optional[torch.Tensor] = None, device="cuda"):
         self.name =  hash_token_chunk
-        self.block_address: int = block_address
+        self.block_address = torch.tensor([block_address], device=device) if block_address is not None else torch.tensor([], device=device)
         self.ref_count: int = 0
         self.last_referenced = float("-inf")
-        self.children: Dict[SequenceStateNode] = []
+        self.children: Dict[SequenceStateNode] = {}
         return
     
     def visit(self, refrence_time: int):
@@ -43,7 +43,7 @@ class PrefixCacheTree():
         self.block_size: int = block_size
         return
     
-    def update_time(self):
+    def _update_time(self):
         self._tree_universal_time += 1
         return
     
@@ -58,7 +58,7 @@ class PrefixCacheTree():
             if current_state_node not in current_node.children:
                 break
             current_node = current_node.children[current_state_node]
-            cached_blocks.append(current_node.block_address)
+            cached_blocks = torch.cat((cached_blocks, current_node.block_address), dim=0)
         if not speculate:
             current_node.visit(self._tree_universal_time)
         return cached_blocks
@@ -90,7 +90,7 @@ class PrefixCacheTree():
             else:
                 #import pdb; pdb.set_trace()
                 raise ValueError(f"Deleting the block of tokens {tokens[i-1*self.block_size: i *self.block_size]} which were never in cache")
-        while elimination_stack[-1].ref_count == 0:
+        while elimination_stack and elimination_stack[-1].ref_count == 0:
             leaf_node = elimination_stack.pop()
             self.eviction_pool[self._tree_universal_time].append(leaf_node)
 
