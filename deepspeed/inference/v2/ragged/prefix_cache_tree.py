c44b50a33533598b39c84998b0fd29ed3c3541c8
Umesh <umchand@microsoft.com>
2024-06-04 20:47:54 +0000

    incremental changes

--- deepspeed/inference/v2/ragged/prefix_cache_tree.py
+++ deepspeed/inference/v2/ragged/prefix_cache_tree.py
@@ -19,10 +19,10 @@ def sequence_to_state(token_ids: Optional[torch.Tensor] = None):
 
 class SequenceStateNode:
     
-    def __init__(self, block_address: Optional[int] = None,
+    def __init__(self, block_addresses: List = [],
                  hash_token_chunk: Optional[torch.Tensor] = None, device="cuda"):
         self.name =  hash_token_chunk
-        self.block_address = torch.tensor([block_address], device=device) if block_address is not None else torch.tensor([], device=device)
+        self.block_addresses = block_addresses
         self.ref_count: int = 0
         self.last_referenced = float("-inf")
         self.children: Dict[SequenceStateNode] = {}
@@ -41,6 +41,7 @@ class PrefixCacheTree():
         self._tree_universal_time = 0
         self.eviction_pool: Dict[int, List] = {}
         self.block_size: int = block_size
+        self.tokens_to_blocks: Dict[str, SequenceStateNode] = {}
         return
     
     def _update_time(self):
@@ -51,31 +52,35 @@ class PrefixCacheTree():
         self._update_time()
         n_blocks = len(tokens) // self.block_size
         current_node = self.root
-        cached_blocks = torch.tensor([], dtype=torch.int32)
+        cached_blocks = [] 
         for i in range(n_blocks):
             chunk = tokens[:(i+1)*self.block_size]
             current_state_node = sequence_to_state(chunk)
-            if current_state_node not in current_node.children:
+            if current_state_node not in self.tokens_to_blocks:
                 break
-            current_node = current_node.children[current_state_node]
-            cached_blocks = torch.cat((cached_blocks, current_node.block_address), dim=0)
-        if not speculate:
-            current_node.visit(self._tree_universal_time)
+            cached_blocks = current_node.block_addresses
+            if not speculate:
+                current_node.visit(self._tree_universal_time)
         return cached_blocks
         
-    def extend(self, tokens: torch.Tensor, new_block_ids: List[int], speculate: bool = False):
+    def extend(self, tokens: torch.Tensor, new_block_ids: List[int], num_already_cached_blocks: int, speculate: bool = False):
         self._update_time()
         n_blocks = len(tokens) // self.block_size
-        current_node = self.root
-        for i in range(n_blocks):
+        if num_already_cached_blocks == 0:
+            current_node = self.root
+        else:
+            current_node = self.tokens_to_blocks[sequence_to_state(tokens[:num_already_cached_blocks])]
+        
+        for i in range(num_already_cached_blocks, n_blocks):
             chunk = tokens[:(i+1)*self.block_size]
             current_state_node = sequence_to_state(chunk)
             if current_state_node not in current_node.children:
-                current_node.children[current_state_node] = SequenceStateNode(address = new_block_ids[i])
+                current_node.children[current_state_node] = SequenceStateNode(block_addresses = current_node.block_addresses +[new_block_ids[i]])
             current_node = current_node.children[current_state_node]
             if not speculate:
                 current_node.visit(self._tree_universal_time)
                 
+    
     def delete(self, tokens: torch.Tensor) -> None:
         n_blocks = len(tokens) // self.block_size
         current_node = self.root
@@ -85,14 +90,26 @@ class PrefixCacheTree():
             current_state_node = sequence_to_state(chunk)
             if current_state_node in current_node.children:
                 current_node = current_node.children[current_state_node]
-                elimination_stack.push(current_node)
+                elimination_stack.append(current_node)
                 current_node.ref_count -= 1
             else:
-                #import pdb; pdb.set_trace()
                 raise ValueError(f"Deleting the block of tokens {tokens[i-1*self.block_size: i *self.block_size]} which were never in cache")
         while elimination_stack and elimination_stack[-1].ref_count == 0:
             leaf_node = elimination_stack.pop()
             self.eviction_pool[self._tree_universal_time].append(leaf_node)
+    
+    def evict(self, num_blocks: int = 1):
+        len_eviction_pool = sum((len(i) for i in self.eviction_pool))
+        if num_blocks <= len_eviction_pool:
+            i = 0
+            while len(self.eviction_pool[i]) <= num_blocks:
+                for node in self.eviction_pool[i]:
+                    del self.tokens_to_blocks[node]
+                    
+                num_blocks = num_blocks - len(self.eviction_pool[i])
+
+        else:
+            raise ValueError(f"Insufficient number of blocks available to be evicted, currently only {len_eviction_pool} blocks can be evicted.")
 
 
         
