cb0b41fe9cd032b0b593ab6bddee910b947a62c7
Masahiro Tanaka <mtanaka@microsoft.com>
2024-06-24 21:08:49 +0000

    disable instrument_w_nvtx

--- deepspeed/utils/nvtx.py
+++ deepspeed/utils/nvtx.py
@@ -10,10 +10,10 @@ def instrument_w_nvtx(func):
     """decorator that causes an NVTX range to be recorded for the duration of the
     function call."""
 
-    def wrapped_fn(*args, **kwargs):
-        get_accelerator().range_push(func.__qualname__)
-        ret_val = func(*args, **kwargs)
-        get_accelerator().range_pop()
-        return ret_val
+    # def wrapped_fn(*args, **kwargs):
+    #     get_accelerator().range_push(func.__qualname__)
+    #     ret_val = func(*args, **kwargs)
+    #     get_accelerator().range_pop()
+    #     return ret_val
 
-    return wrapped_fn
+    return func
