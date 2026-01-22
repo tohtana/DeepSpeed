tohtana/DeepSpeed

commit f897b67394827e2bc18a354603470d45b7e687ae

fix #7188  (#7371)
I found that when using DeepSpeed Zero2 for my training task, the loss
becomes 0 at the third step with a grad_norm of 1.414. This issue
doesn't occur when using Zero3. I found the same issue #7188. After
conducting a series of experiments, I identified the cause: there's a
synchronization problem when using double ipg_buffer swapping. The issue
was resolved after making modifications.

before 

![image](https://github.com/user-attachments/assets/981d0829-e15f-4899-ae2c-4eca16ef138d)

after

![image](https://github.com/user-attachments/assets/8b6b8403-d5df-4aa8-b573-195b9ee1fdfb)

Signed-off-by: vinceliu <lpnpcs@gmail.com>
Co-authored-by: Logan Adams <114770087+loadams@users.noreply.github.com>
Co-authored-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>
Co-authored-by: Hongwei Chen <33092912+hwchen2017@users.noreply.github.com>

deepspeed/runtime/zero/stage_1_and_2.py
@@ -995,6 +995,9 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
             self.report_ipg_memory_usage("In ipg_remove_grads before reduce_ipg_grads", param.numel(), param.dtype)
             self.reduce_ipg_grads()
             if self.contiguous_gradients and self.overlap_comm:
+                if not get_accelerator().resolves_data_dependency():
+                    self.reduction_stream.wait_stream(get_accelerator().current_stream())
+                    get_accelerator().current_stream().wait_stream(self.reduction_stream)
                 # Swap index between 0 and 1
                 bucket.index = 1 - bucket.index
             self.report_ipg_memory_usage("In ipg_remove_grads after reduce_ipg_grads", param.numel(), param.dtype)

commit 15ad92b459c6c39b7c5527efe1e42080eb4ab99f

Fix ping-pong buffer index reset and removing redundant stream sync (#7805)
Fix #7804  and  #7188

After investigating the code in
`deepspeed/runtime/zero/stage_1_and_2.py`, I have identified the root
cause. The regression regarding communication overlap was introduced in
PR #7371 (https://github.com/deepspeedai/DeepSpeed/pull/7371). While the
additional two-stream synchronization in that PR fixes gradient
corruption, it effectively disables the overlapping behavior.

The underlying issue causing the gradient corruption (which #7371
attempted to fix) was actually introduced in PR #6993
(https://github.com/deepspeedai/DeepSpeed/pull/6993). In that PR,
`bucket.clear()` incorrectly resets the ping-pong buffer index to 0 at
the end of `reduce_ipg_grads`. This logic disrupts the buffer index
swapping mechanism within
`reduce_independent_p_g_buckets_and_remove_grads`.

To fix this, L121 in `deepspeed/runtime/zero/stage_1_and_2.py` should be
removed to prevent resetting the buffer index. Additionally, the stream
synchronization logic introduced in #7371 should be removed to restore
the `overlap_comm=True` functionality.

---------

Signed-off-by: szlent <metarufolds@gmail.com>
Signed-off-by: Masahiro Tanaka <mtanaka@anyscale.com>
Co-authored-by: Masahiro Tanaka <mtanaka@anyscale.com>

deepspeed/runtime/zero/stage_1_and_2.py
@@ -118,7 +118,6 @@ class IPGBucket:
         self.params.clear()
         self.grads.clear()
         self.elements = 0
-        self.index = 0
         self.has_moe_params = False
 
 
@@ -1052,11 +1051,8 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
         bucket = self.ipg_buckets[comm_dtype]
         if bucket.elements + param.numel() > self.reduce_bucket_size:
             self.report_ipg_memory_usage("In ipg_remove_grads before reduce_ipg_grads", param.numel())
-            self.reduce_ipg_grads()
+            self.reduce_ipg_grads(comm_dtype=comm_dtype)
             if self.contiguous_gradients and self.overlap_comm:
-                if not get_accelerator().resolves_data_dependency():
-                    self.reduction_stream.wait_stream(get_accelerator().current_stream())
-                    get_accelerator().current_stream().wait_stream(self.reduction_stream)
                 # Swap index between 0 and 1
                 bucket.index = 1 - bucket.index
             self.report_ipg_memory_usage("In ipg_remove_grads after reduce_ipg_grads", param.numel())
@@ -1500,8 +1496,11 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
         #print(f"Grad norm after copy to contiguous_buffer {param.grad.data.norm()}")
         self.grads_in_partition_offset += param.numel()
 
-    def reduce_ipg_grads(self):
-        for comm_dtype in sort_dtypes(self.ipg_buckets.keys()):
+    def reduce_ipg_grads(self, comm_dtype=None):
+        dtypes = sort_dtypes(self.ipg_buckets.keys())
+        if comm_dtype is not None:
+            dtypes = [comm_dtype]
+        for comm_dtype in dtypes:
             bucket = self.ipg_buckets[comm_dtype]
 
             if self.contiguous_gradients:
@@ -1536,7 +1535,7 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
             stream = get_accelerator().current_stream()
 
         with get_accelerator().stream(stream):
-            for comm_dtype in sort_dtypes(self.ipg_buckets.keys()):
+            for comm_dtype in dtypes:
                 bucket = self.ipg_buckets[comm_dtype]
 
                 for group_idx, param_idx_in_group, param_id in bucket.params: