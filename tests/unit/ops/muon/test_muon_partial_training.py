a4046b69479f983e42bb27ea864045d328676a96
iLeGend <824040212@qq.com>
2026-08-19 23:20:48 +0800

    Remove unused import of DeepSpeedEngine and update optimizer selection method in test
    Signed-off-by: iLeGend <824040212@qq.com>

--- tests/unit/ops/muon/test_muon_partial_training.py
+++ tests/unit/ops/muon/test_muon_partial_training.py
@@ -26,7 +26,6 @@ import deepspeed
 import pytest
 from unit.common import DistributedTest
 from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam, ZenFlowCPUAdam
-from deepspeed.runtime.engine import DeepSpeedEngine
 from deepspeed.runtime.zero.muon.muon_optimizer import MuonWithAuxAdam
 
 
@@ -395,7 +394,7 @@ class _AdamSelectionEngine:
     ],
 )
 def test_adam_backend_selection(engine, parameters, adam_w_mode, expected_class, expected_kwargs):
-    optimizer_class, optimizer_kwargs = DeepSpeedEngine._select_adam_optimizer(engine, parameters, adam_w_mode)
+    optimizer_class, optimizer_kwargs = engine._select_adam_optimizer(parameters, adam_w_mode)
 
     assert optimizer_class is expected_class
     assert optimizer_kwargs == expected_kwargs
